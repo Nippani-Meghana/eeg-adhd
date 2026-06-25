a5d8ef3b49c8f42a2928063f1ce67c9c01b3a577
Nippani <meghananippani@gmail.com>
2026-06-25 19:35:57 +0530

    Plotted Fp1 channel of the first subject in the dataset.

--- signal_processing/testing.py
+++ signal_processing/testing.py
@@ -13,4 +13,12 @@ subject_id = all_subjects[0]
 subject_data, class_label = nasarbadi_helper.get_subject_info(subject_id)
 
 
+#Plot the signal of the first channel
+plt.figure(figsize=(12, 6))
+subject_data.iloc[:, 0].plot(title=f"Subject ID: {subject_id}, Class: {class_label}, Channel: {subject_data.columns[0]}")
+plt.xlabel("Time")
+plt.ylabel("Amplitude")
+plt.show()
+
+
 
